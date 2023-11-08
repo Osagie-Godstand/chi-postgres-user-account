240cbc341e7452b4247f7c55406caaad2fa9ec61
Osagie-Godstand <51753700+Osagie-Godstand@users.noreply.github.com>
2023-11-08 01:22:47 +0000

    chi-postgres-user-account

--- handlers/user_handler.go
+++ handlers/user_handler.go
@@ -8,7 +8,7 @@ import (
 	"net/http"
 
 	"github.com/Osagie-Godstand/chi-postgres-user-account/models"
-	"github.com/Osagie-Godstand/chi-user-account-crelog/db"
+	"github.com/Osagie-Godstand/chi-postgres-user-account/db"
 	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
 )
